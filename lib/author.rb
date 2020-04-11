0f69410de763178f18ac389765de541323adf638
chris brady <chrisb3546@yahoo.com>
2020-04-11 18:14:38 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,13 @@
+class Author 
+  
+    
+ attr_accessor :name
+
+def initialize
+  @name = name 
+end 
+
+author = Author.new 
+author.name = ("2")
+
+end
---
+++ lib/post.rb
@@ -0,0 +1,14 @@
+class Post 
+  
+    
+ attr_accessor :title, :author
+
+def initialize
+  @title = title
+end 
+
+post = Post.new 
+post.title = ("Hello World")
+post.author = ("2")
+
+end
