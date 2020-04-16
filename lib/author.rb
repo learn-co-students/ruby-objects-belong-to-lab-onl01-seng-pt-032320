baeec3ccd0c2f8120bd01f6ec91811ba7b736580
Evan Smith <professionalemail@zoho.com>
2020-04-16 20:13:06 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,3 @@
 class Author
   attr_accessor :name
-  def initialize(name="Uncle Bob")
-    @name = name
-  end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,3 @@
 class Song
   attr_accessor :title, :artist
-  def initialize(title="7/11")
-    @title = title
-  end
 end
